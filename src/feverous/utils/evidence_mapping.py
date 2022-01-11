e03f50e0f2c867d7545ffe15ca8e5d976ddb4fdd
sushilk1997 <iamsk1304@gmail.com>
2022-01-11 17:38:01 +0530

    check evidence mapping

---
+++ src/feverous/utils/evidence_mapping.py
@@ -0,0 +1,26 @@
+from .wiki_window import * 
+from .wiki_sentence import *
+from .wiki_page import *
+
+
+data_path = "/mnt/infonas/data/sushilk/MTP/FEVER/FEVEROUS-main/data/train.jsonl"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
